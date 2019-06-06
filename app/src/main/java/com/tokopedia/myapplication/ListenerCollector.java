66da7bc334230723408b6c07c20594993dd65928
hendrysetiadi89 <hendry.setiadi.89@gmail.com>
2019-06-06 19:26:46 +0700

    Add Leak Canary

--- app/src/main/java/com/tokopedia/myapplication/ListenerCollector.java
+++ app/src/main/java/com/tokopedia/myapplication/ListenerCollector.java
@@ -2,6 +2,7 @@ package com.tokopedia.myapplication;
 
 import android.view.View;
 
+import java.util.HashMap;
 import java.util.WeakHashMap;
 
 /**
@@ -11,7 +12,7 @@ import java.util.WeakHashMap;
 public class ListenerCollector {
     // A common case is to want to store all the listeners for a particular type of view
     // somewhere.  Harmless AND convenient.  Or... is it? o_0
-    static private WeakHashMap<View, MyCustomView.MyListener> sListeners = new WeakHashMap<View, MyCustomView.MyListener>();
+    static private HashMap<View, MyCustomView.MyListener> sListeners = new HashMap<View, MyCustomView.MyListener>();
 
     public void setListener(View view, MyCustomView.MyListener listener) {
         sListeners.put(view, listener);
